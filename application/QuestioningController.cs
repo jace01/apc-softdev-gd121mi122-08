72d2a4e6917c65e441670373d43a153a2c814887
jrdeguzman <jpaolo.deguzman@gmail.com>
2015-09-01 08:06:06 +0800

    Put randomize numbers in separate function. Still not working.

--- application/QuestioningController.cs
+++ application/QuestioningController.cs
@@ -18,14 +18,10 @@ public class QuestioningController : MonoBehaviour {
 	void Update () {
 		if(Input.GetButtonDown("Fire1")) {		//activates upon touch
 			if(arraypos < 3) {
-				xpos = Random.Range(-4,0);		//randomize positions within an area
-				ypos = Random.Range(-1, 2);
-				this.transform.position = new Vector2(xpos, ypos);
+				Shuffle ();
+
 				//Note: pa-adjust na lang ang mga ranges according sa screen size ng Unity mo.
-
-
 				Popup ();
-				arraypos++;
 			}
 			else {
 				Debug.Log ("End of questioning");
@@ -34,7 +30,14 @@ public class QuestioningController : MonoBehaviour {
 		}
 	}
 
+	public void Shuffle () {
+		xpos = Random.Range(-4,6);		//randomize positions within an area
+		ypos = Random.Range(-1, 6);
+		this.transform.position = new Vector3(xpos, ypos, -10);
+	}
+
 	public void Popup () {
 		Instantiate (bubbles [arraypos], bubbles[arraypos].transform.position, this.transform.rotation);
+		arraypos++;
 	}
 }

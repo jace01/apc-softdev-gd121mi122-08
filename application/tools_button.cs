d1ee078d8e299f9dfec713de700eeb4ad548460b
Joyce Miralles <joytmae01@gmail.com>
2015-09-03 17:25:53 +0800

    Re-upload dialogue window and added new files.

---
+++ application/tools_button.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tools_button : MonoBehaviour {
+	
+	RuntimePlatform platform = Application.platform;
+	//public GameObject yes;
+	
+	// Update is called once per frame
+	void Update () {
+		if(platform == RuntimePlatform.Android || platform == RuntimePlatform.IPhonePlayer){
+			if(Input.touchCount > 0) {
+				if(Input.GetTouch(0).phase == TouchPhase.Began){
+					checkTouch(Input.GetTouch(0).position);
+				}
+			}
+		}else if(platform == RuntimePlatform.WindowsEditor){
+			if(Input.GetMouseButtonDown(0)) {
+				checkTouch(Input.mousePosition);
+			}
+		}
+	}
+	
+	void checkTouch(Vector3 pos){
+		Vector3 wp = Camera.main.ScreenToWorldPoint(pos);
+		Vector2 touchPos = new Vector2(wp.x, wp.y);
+		var hit = Physics2D.OverlapPoint (touchPos);
+		
+		if(hit == this.gameObject.GetComponent<Collider2D>()){
+			Application.LoadLevel("tools2");
+		}
+	}
+}
